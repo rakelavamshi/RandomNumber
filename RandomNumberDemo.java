e787dde03a6f5cc2344ed1923b6d53201c4ea615
Vamshi Rakela <vamshirakela@gmail.com>
2018-01-01 21:53:53 +0530

    Add files via upload

---
+++ RandomNumberDemo.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+
+public class RandomNumberDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		Random r=new Random();
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(r.nextGaussian());
+		}
+
+	}
+
+}
